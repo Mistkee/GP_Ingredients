efdb2504655fb48d4b12a858e1bd050a853144dc
Mistkee <153066782+Mistkee@users.noreply.github.com>
2024-03-13 15:04:46 +0100

    [fix] cauldron script

--- Assets/Scripts/C_Scripts/C_CauldronScript.cs
+++ Assets/Scripts/C_Scripts/C_CauldronScript.cs
@@ -7,7 +7,12 @@ public class C_Cauldron : Interactive
     [SerializeField] KeyItemData item;
     public override void OnInteraction()
     {
+        foreach (KeyItemData item in requiredItems)
+        {
+            Inventory.Instance.RemoveFromInventory(item);
+        }
         //GetComponent<Animator>().SetTrigger("Potion") ;
         Inventory.Instance.PickupKeyItem(item);
+        
     }
 }
